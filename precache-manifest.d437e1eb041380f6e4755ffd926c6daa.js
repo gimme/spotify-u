f14a7eebcbe456459693cc49bbbb908588776f5f
Anders Magnusson <anders14magnusson@gmail.com>
2020-06-01 15:11:06 +0200

    Updates

---
+++ precache-manifest.d437e1eb041380f6e4755ffd926c6daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea352e62e62c81b4d9566f6e9b3a2a68",
+    "url": "/SpotifyU/index.html"
+  },
+  {
+    "revision": "6c926c2929e9e2d5d95b",
+    "url": "/SpotifyU/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ca542bf9344843befd5",
+    "url": "/SpotifyU/static/js/2.9b93141b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpotifyU/static/js/2.9b93141b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c926c2929e9e2d5d95b",
+    "url": "/SpotifyU/static/js/main.fe5cd742.chunk.js"
+  },
+  {
+    "revision": "26d30fa9abd8e7977320",
+    "url": "/SpotifyU/static/js/runtime-main.05ed6356.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SpotifyU/static/media/logo.5d5d9eef.svg"
+  }
+]);
